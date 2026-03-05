16eb825b0d298356f4f0d17b1198addab0c4a646
ckcfeenstra <ckcfeenstra@gmail.com>
2026-03-05 13:43:53 -0600

    [Lab 6] [Lab 06] Update from StudySite.ai

--- main.cpp
+++ main.cpp
@@ -7,6 +7,11 @@ int main() {
     float B_points = 0.00;
     float C_points = 0.00;
     float D_points = 0.00;
+
+    float assignment_score = 0.00;
+    char earned_grade;
+    bool score_input = true;
+    int assignment = 1;
     
     float total_points_earned = 0.00;
     float total_percentage_earned = 0.00;
@@ -42,34 +47,51 @@ int main() {
     std::cout << "Points needed for an 'A': " << A_points << std::endl;
     std::cout << "Points needed for a 'B': " << B_points << std::endl;
     std::cout << "Points needed for a 'C': " << C_points << std::endl;
-    std::cout << "Points needed for a 'D': " << D_points << std::endl;
+    std::cout << "Points needed for a 'D': " << D_points << std::endl << '\n';
 
     // Grade Calculation
-    std::cout << "\nGrade Calculation\n";
-    std::cout << "Please input the total of all points earned in the course: ";
-    std::cin >> total_points_earned;
-    std::cout << '\n';
+    std::cout << "Grade Calculation\n\n";
+    std::cout << "You will be prompted to input scores for all assignments.\n";
+    std::cout << "(Input a negative number to cease the prompt and calculate a letter grade.)\n\n";
+
+    while (score_input) {
+        std::cout << "Please enter your score for Assignment " << assignment << ": ";
+        std::cin >> assignment_score;
+
+        if (assignment_score >= 0) {
+            total_points_earned += assignment_score;
+            assignment++;
+        } else {
+            score_input = false;
+        }
+    }
 
     // Determine the final grade
     if (total_points_earned >= A_points) {
-        std::cout << "You earned a final grade of: 'A' at: ";
+        earned_grade = 'A';
     }
     else if (total_points_earned >= B_points) {
-        std::cout << "You earned a final grade of: 'B' at: ";
+        earned_grade = 'B';
     }
     else if (total_points_earned >= C_points) {
-        std::cout << "You earned a final grade of: 'C' at: ";
+        earned_grade = 'C';
     }
     else if (total_points_earned >= D_points) {
-        std::cout << "You earned a final grade of: 'D' at: ";
+        earned_grade = 'D';
     }
     else {
-        std::cout << "You earned a final grade of: 'F' at: ";
+        earned_grade = 'F';
     }
 
     // Calculate and display percentage
     total_percentage_earned = (total_points_earned / total_course_points) * 100;
-    std::cout << total_percentage_earned << '%' << '\n';
+
+    //Display Final Results
+    std::cout << "\nFinal Results\n";
+    std::cout << "Total Points Earned: " << total_points_earned << '\n';
+    std::cout << "Total Points Possible: " << total_course_points << '\n';
+    std::cout << "Total Percentage: " << total_percentage_earned << '%' << '\n';
+    std::cout << "Final Letter Grade: " << earned_grade << '\n';
 
     return 0;
 }
